78eb860481295ab595704114714233fe5576f778
hydai <z54981220@gmail.com>
2016-10-29 17:04:50 +0800

    We can move snake by keyboard Up, Down, Left, and right.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ struct Point<T> {
 }
 
 fn main() {
-    println!("Hello, world!");
     let mut window: PistonWindow =
         WindowSettings::new("Meow", WINDOW_SIZE)
             .exit_on_esc(true).build().unwrap();
@@ -24,12 +23,23 @@ fn main() {
             y: 200.0
         };
 
-    while let Some(e) = window.next() {
-        window.draw_2d(&e, |c, g| {
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |c, g| {
             clear(BG_COLOR, g);
             rectangle(SNAKE_COLOR,
                       [snake_pos.x, snake_pos.y, 100.0, 100.0],
                       c.transform, g);
         });
+
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            println!("{:?}", key);
+            match key {
+                Key::Up => snake_pos.y -= 1.0,
+                Key::Down => snake_pos.y += 1.0,
+                Key::Left => snake_pos.x -= 1.0,
+                Key::Right => snake_pos.x += 1.0,
+                _ => {},
+            }
+        }
     }
 }
